6d303bfb38fb11ce19971adac654e511c9a98e2a
Kamal Allouzi <31010418+kamalallouzi@users.noreply.github.com>
2022-03-12 04:00:32 -0600

    heroku

--- twitterEmotions/settings.py
+++ twitterEmotions/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1dmiie37m@e$bn!s_r&tnq1%lcjdk#rg41&=sqy=!4(0hb!ji2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://twitteremotionapp.herokuapp.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
