5763c310f0f716f649decde94f40682e71d26855
Kamal Allouzi <31010418+kamalallouzi@users.noreply.github.com>
2022-03-12 17:35:10 -0600

    settings

--- twitterEmotions/settings.py
+++ twitterEmotions/settings.py
@@ -116,8 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
